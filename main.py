e86cb3336b051775d9f5457e84f9553a16a51bbb
mothlovesfire <22015brian@gmail.com>
2025-10-24 22:08:15 -0400

    Added main.py, containing everything required for a KNN implementation

---
+++ main.py
@@ -0,0 +1,127 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
+'''
+    standardize()
+        Helper function to standardize/normalize an array of data.
+        Input an array.
+        Returns an equally-sized array with standardized values.
+'''
+#   Helper function to standardize an array of data
+def standardize(inputArr):
+    mean = inputArr.mean(axis = 0)
+    std = inputArr.std(axis = 0)
+    std[std == 0] = 1
+    return (inputArr - mean) / std
+
+'''
+    fivefold()
+        Helper function to divide an array into 5 equally sized parts.
+        Input an array.
+        Returns an array of 5 arrays, splitting the original array.
+        (Useful for five-fold validation, a replacement to random validation sets.)
+'''
+#   Helper function to divide an array into 5 equal parts
+def fivefold(inputArr):
+    height = inputArr.shape[0]
+    folds = []
+    for i in range(5):
+        folds.append(inputArr[(int) (height/5 * i) : (int) (height/5 * (i + 1))])
+    return folds
+
+'''
+    plotimg()
+        Helper function to print out a specified range of images and their associated keypoints.
+        Input an array representation of an image, an array representation of keypoints, and a tuple containing the desired range.
+        Returns nothing, but displays all images within that range in a new window.
+        (Similar to Homework 1, useful for checking out what's going on.)
+'''
+#   Helper function to print out a specified range of images and their associated keypoints
+def plotimg(imageArr, keypoints, indexrange):
+    rangeDiff = indexrange[1] - indexrange[0]
+    fig, ax = plt.subplots(1, rangeDiff, figsize=(5, 5))
+    if (rangeDiff == 1):
+        ax = [ax]
+    for increment in range(rangeDiff):
+        index = indexrange[0] + increment
+        image = imageArr[index].reshape((96, 96))
+        x_coords = keypoints[index][0::2]
+        y_coords = keypoints[index][1::2]
+
+        ax[increment].imshow(image, cmap=plt.get_cmap("gray"))
+        ax[increment].set_title(index)
+        ax[increment].scatter(x_coords, y_coords)
+        increment += 1
+    plt.show()
+
+
+#   Loading data
+print("Loading data...")
+train_frame = pd.read_csv('data\\training.csv').dropna()
+test_frame = pd.read_csv('data\\test.csv')
+
+print("Reformatting data...")
+#print(train_frame.head(1))
+train_points = train_frame.values[:, : -1]
+train_images_str = train_frame.values[:, -1]
+#print(train_points.shape)
+#print(train_image.shape)
+#print(train_image.shape[0])
+train_images = []
+for i in range(train_images_str.shape[0]):
+    train_images.append(np.array(train_images_str[i].split(), dtype = float))
+train_images = np.array(train_images)
+train_points = np.array(train_points.astype(float))
+
+print("Printing images...")
+plotimg(train_images, train_points, (0, 5))
+
+print("Dividing data...")
+train_points_folds = fivefold(train_points)
+train_image_folds = fivefold(train_images)
+
+print("Started training (5-fold validation).")
+train_predictions = []
+for iter in range(5):
+    print(f"Starting fold {iter + 1}/5...")
+    train_trainfolds_points = []
+    train_trainfolds_images = []
+    train_validfolds_points = []
+    train_validfolds_images = []
+    for i in range(5):
+        if (iter != i):
+            train_trainfolds_images.append(train_image_folds[i])
+            train_trainfolds_points.append(train_points_folds[i])
+        else:
+            train_validfolds_images = train_image_folds[i]
+            train_validfolds_points = train_points_folds[i]
+    train_trainfolds_images = np.vstack(train_trainfolds_images)
+    train_trainfolds_points = np.vstack(train_trainfolds_points)
+
+    #print(train_trainfolds_points.shape)
+    #print(train_trainfolds_images.shape)
+    #print(train_validfolds_points.shape)
+    #print(train_validfolds_images.shape)
+
+    for v in train_validfolds_images:
+        distances = np.sum((train_trainfolds_images - v) ** 2, axis = 1)
+        sortedindices = np.argsort(distances)
+        nearestpoints = []
+        for i in range(10):
+            nearestpoints.append(train_trainfolds_points[sortedindices[i]])
+        nearestpoints = np.array(nearestpoints)
+        avgpoints = nearestpoints.mean(axis = 0)
+        train_predictions.append(avgpoints)
+    
+
+print("Training finished!")
+train_predictions = np.array(train_predictions)
+
+print("Metrics:")
+print(f"train_points.shape: {train_points.shape}")
+print(f"train_predictions.shape: {train_predictions.shape}")
+print(f"MSE: {mean_squared_error(train_points, train_predictions)}")
+print(f"MAE: {mean_absolute_error(train_points, train_predictions)}")
